a9874fc5757caaa6dd43d44b3791cf379c018275
nitko12 <nitko12@hotmail.com>
2022-01-23 20:17:17 +0100

    uart control

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,43 @@ uint8_t pos[] = {10, 20, 30, 40}; // [10, 40]
 
 static inline void update_servo(uint8_t pin, uint8_t pos) // pin = (1 << n), pos: [0, 40]
 {
-    SPI.transfer(0);
+    SPI.transfer(~pin);
     _delay_us(100);
 
-    SPI.transfer(pin);
+    SPI.transfer(0xFF);
 
     _delay_us(50);
     for (int j = 0; j < pos; ++j)
         _delay_us(50);
 
-    SPI.transfer(0);
-    _delay_us(18000);
+    SPI.transfer(~pin);
+    _delay_us(100);
 
     SPI.transfer(0xFF);
+}
+
+static inline void initTimer1(void)
+{
+    TCCR1 |= (1 << CTC1);                             // clear timer on compare match
+    TCCR1 |= (1 << CS13) | (1 << CS12) | (1 << CS11); // clock prescaler 8192
+    // OCR1C = 122;                                                    // compare match value
+    TIMSK |= (1 << OCIE1A); // enable compare match interrupt
+}
+
+volatile uint8_t i = 0, block = 0;
+ISR(TIMER1_COMPA_vect)
+{
+    if (block)
+        return;
+
+    if (i == len(servos))
+        i = 0;
+    else
+        i++;
+
+    // if (i == 0)
+    //     pos[i] = (pos[i] - 10 + 10) % 30 + 10;
+    update_servo(servos[i], pos[i]);
 }
 
 int main()
@@ -30,25 +54,31 @@ int main()
     SPI.begin();
     SPI.transfer(0xFF);
 
-    DDRB |= (1 << PB4);
+    initTimer1();
+    sei();
 
+    uint8_t t, j = 0;
     while (1)
     {
-        uint8_t c;
-        while (c = RxByte())
+        if (t = RxByte())
         {
+            block = 1;
+            if (t == '0')
+            {
+                pos[0] = (pos[0] - 10 + 15) % 30 + 10;
+                update_servo(servos[0], pos[0]);
+            }
+            if (t == '1')
+            {
+                pos[1] = (pos[1] - 10 + 15) % 30 + 10;
+                update_servo(servos[1], pos[1]);
+            }
+            if (t == '2')
+                pos[2] = (pos[2] - 10 + 15) % 30 + 10;
+            if (t == '3')
+                pos[3] = (pos[3] - 10 + 15) % 30 + 10;
 
-            pos[0] -= 10;
-
-            if (c == '0')
-                pos[0] = (pos[0] - 5 + 30) % 30;
-            if (c == '1')
-                pos[0] = (pos[0] + 5) % 30;
-
-            pos[0] += 10;
-
-            for (uint8_t i = 0; i < len(servos); ++i)
-                update_servo(servos[i], pos[i]);
+            block = 0;
         }
     }
 
